e3cdfef9c5857570a9d1646443f171955093682f
kjakubek <kjakubek@seattleu.edu>
2020-08-09 11:33:53 -0700

    Update 01_sorting_data.R

--- code/01_sorting_data.R
+++ code/01_sorting_data.R
@@ -9,16 +9,24 @@ library(haven)
 # Organize + Read in Data ----
 
 #     Reads in data for WINDOWS
-survey_info_1 <- read_dta("raw_data/glss4_new/glss4_new/sec0a.dta")
-
+survey_info_a <- read_dta("raw_data/glss4_new/glss4_new/sec0a.dta")
 hh <- read_dta("raw_data/glss4_new/glss4_new/sec1.dta")
+education <- read_dta("raw_data/glss4_new/glss4_new/sec2a.dta")
+edu_career <- read_dta("raw_data/glss4_new/glss4_new/sec2b.dta")
+edu_lit <- read_dta("raw_data/glss4_new/glss4_new/sec2c.dta")
 
-#   Reads in data for OS
-#     Uses the same variable names as those imported for WINDOWS
+agriculture <- read_dta("raw_data/glss4_new/glss4_new/sec8a1.dta")
+
+agg2 <- read_dta("raw_data/glss4_new/glss4_new/aggregates/agg2.dta")
+subagg2 <- read_dta("raw_data/glss4_new/glss4_new/aggregates/subagg2.dta")
+
+#   Reads in data for OS -- Brittney please check if this works
+#   Uses the same variable names as those imported for WINDOWS
 
 hh <- read_dta("raw_data/glss4_new/__MACOSX/glss4_new/._sec1.dta")
 
-variables <- names(survey_info_1)
+# Check Variable names
+variables <- names(survey_info_a)
 
 variables <- names(hh)
 
