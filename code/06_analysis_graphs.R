d5aee4b13555412735df0ba7077f23afe91dfad0
kjakubek <kjakubek@seattleu.edu>
2020-09-03 20:06:13 -0700

    Linear Models + log of forest

--- code/06_analysis_graphs.R
+++ code/06_analysis_graphs.R
@@ -14,64 +14,13 @@ summary(rev_hh_agri_profit)
 cor(select(rev_hh_agri_profit, -c(nh, region_district, eco_zone, urban_rural,
                                   education_level, read_max, write_max)))
 
-# LINEAR Models
 
-# Linear model with savannah_zone and rural zone
-model_savanah_lm <- lm(data =rev_hh_agri_profit,
-                   profit_per_rope ~
-                     +                     savannah_zone+rural
-                   +  factor(education_max )+ factor(read_max) +factor (write_max) + factor (calc_max) +   # Education
-                     +                      road + bank + daily_market + periodic_market +      # Community economy
-                     +                      prim_school + jss_school +                          # Community education
-                     +                      agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid)# Community
+# Linear model for any eco-zone
+model_lm <- lm(data = rev_hh_agri_profit,
+                   profit_per_rope ~ factor(education_max)  + sharecroppers)
 
-summary(model_all_lm) # view summary of linear model
+summary(model_lm) # view summary of linear model
 
-
-# Linear model with coastal_zone and rural zone
-model_coastal_lm <- lm(data = rev_hh_agri_profit,
-                   profit_per_rope ~
-                     +                     coastal_zone+rural
-                   +  factor(education_max )+ factor(read_max) +factor (write_max) + factor (calc_max) +   # Education
-                     +                      road + bank + daily_market + periodic_market +      # Community economy
-                     +                      prim_school + jss_school +                          # Community education
-                     +                      agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid)# Community
-
-summary(model_all_lm) # view summary of linear model
-
-
-# Linear model with forest_zone and rural zone
-model_forest_lm <- lm(data = rev_hh_agri_profit,
-                   profit_per_rope ~
-                     +                     forest_zone+rural
-                   +  factor(education_max )+ factor(read_max) +factor (write_max) + factor (calc_max) +   # Education
-                     +                      road + bank + daily_market + periodic_market +      # Community economy
-                     +                      prim_school + jss_school +                          # Community education
-                     +                      agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid)# Community
-
-summary(model_all_lm) # view summary of linear model
-
-# LOG MODELS
-# Log model with savannah_zone and rural zone
-model_savannah_log <- lm(data = rev_hh_agri_profit,
-                         log(profit_per_rope) ~            # Location
-                         + factor(education_max)           # Education
-                                                           # Community economy
-                         + prim_school + jss_school        # Community education
-                         + community_coop + sharecroppers) # Community
-
-summary(model_savannah_log)
-
-
-# Log model with coastal_zone and rural zone
-model_coastal_log <- lm(data = rev_hh_agri_profit,
-                        log(profit_per_rope) ~ coastal_zone # Location
-                        + factor(education_max)             # Education
-                        + bank                              # Community economy
-                                                            # Community education
-                        + community_coop + sharecroppers)   # Community
-
-summary(model_coastal_log)
 
 
 # Log model with forest_zone and rural zone
@@ -122,6 +71,7 @@ model_forest_lm <- lm(data = rev_hh_agri_profit,
                       + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
 
 summary(model_forest_lm)
+
 
 
 # LOG profit_per_rope - ALL VARIABLES
