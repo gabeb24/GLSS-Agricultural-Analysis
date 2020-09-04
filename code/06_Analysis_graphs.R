8f6bc4671c7629fd603a66a2aa85328f26caeb02
kjakubek <kjakubek@seattleu.edu>
2020-09-03 18:06:18 -0700

    Models w/ Outliers removed

--- code/06_Analysis_graphs.R
+++ code/06_Analysis_graphs.R
@@ -9,34 +9,34 @@
 # LINEAR Profit_per rope
 
 #Linear model with savannah_zone and rural zone
-model_savannah_lm <- lm(data = hh_agri_profit,
-                   profit_per_rope ~ savannah_zone + rural               # Location
-                     + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
-                     + road + bank + daily_market + periodic_market      # Community economy
-                     + prim_school + jss_school                          # Community education
-                     + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
+model_savannah_lm <- lm(data = rev_hh_agri_profit,
+                        profit_per_rope ~ savannah_zone + rural               # Location
+                        + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
+                        + road + bank + daily_market + periodic_market      # Community economy
+                        + prim_school + jss_school                          # Community education
+                        + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
 
 summary(model_savannah_lm)
 
 
 #Linear model with coastal_zone and rural zone
-model_coastal_lm <- lm(data = hh_agri_profit,
-                        profit_per_rope ~ coastal_zone + rural               # Location
-                        + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
-                        + road + bank + daily_market + periodic_market      # Community economy
-                        + prim_school + jss_school                          # Community education
-                        + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
+model_coastal_lm <- lm(data = rev_hh_agri_profit,
+                       profit_per_rope ~ coastal_zone + rural               # Location
+                       + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
+                       + road + bank + daily_market + periodic_market      # Community economy
+                       + prim_school + jss_school                          # Community education
+                       + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
 
 summary(model_coastal_lm)
 
 
 #Linear model with forest_zone and rural zone
-model_forest_lm <- lm(data = hh_agri_profit,
-                        profit_per_rope ~ forest_zone + rural               # Location
-                        + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
-                        + road + bank + daily_market + periodic_market      # Community economy
-                        + prim_school + jss_school                          # Community education
-                        + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
+model_forest_lm <- lm(data = rev_hh_agri_profit,
+                      profit_per_rope ~ forest_zone + rural               # Location
+                      + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
+                      + road + bank + daily_market + periodic_market      # Community economy
+                      + prim_school + jss_school                          # Community education
+                      + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
 
 summary(model_forest_lm)
 
@@ -44,34 +44,34 @@ summary(model_forest_lm)
 # LOG profit_per_rope
 
 # Log model with savannah_zone and rural zone
-model_savannah_log <- lm(data = hh_agri_profit,
-                        log(profit_per_rope) ~ savannah_zone + rural               # Location
-                        + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
-                        + road + bank + daily_market + periodic_market      # Community economy
-                        + prim_school + jss_school                          # Community education
-                        + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
+model_savannah_log <- lm(data = rev_hh_agri_profit,
+                         log(profit_per_rope) ~ savannah_zone + rural               # Location
+                         + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
+                         + road + bank + daily_market + periodic_market      # Community economy
+                         + prim_school + jss_school                          # Community education
+                         + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
 
 summary(model_savannah_log)
 
 
 # Log model with coastal_zone and rural zone
-model_coastal_log <- lm(data = hh_agri_profit,
-                       log(profit_per_rope) ~ coastal_zone + rural               # Location
-                       + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
-                       + road + bank + daily_market + periodic_market      # Community economy
-                       + prim_school + jss_school                          # Community education
-                       + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
+model_coastal_log <- lm(data = rev_hh_agri_profit,
+                        log(profit_per_rope) ~ coastal_zone + rural               # Location
+                        + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
+                        + road + bank + daily_market + periodic_market      # Community economy
+                        + prim_school + jss_school                          # Community education
+                        + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
 
 summary(model_coastal_log)
 
 
 # Log model with forest_zone and rural zone
-model_forest_log <- lm(data = hh_agri_profit,
-                      log(profit_per_rope) ~ forest_zone + rural               # Location
-                      + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
-                      + road + bank + daily_market + periodic_market      # Community economy
-                      + prim_school + jss_school                          # Community education
-                      + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
+model_forest_log <- lm(data = rev_hh_agri_profit,
+                       log(profit_per_rope) ~ forest_zone + rural               # Location
+                       + factor(education_max) + factor(read_max) +factor (write_max) + factor (calc_max)      # Education
+                       + road + bank + daily_market + periodic_market      # Community economy
+                       + prim_school + jss_school                          # Community education
+                       + agg_ext_center + community_coop + irrigated_fields + sharecroppers + farm_mutual_aid) # Community
 
 summary(model_forest_log)
 
